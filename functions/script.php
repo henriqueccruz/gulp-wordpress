48265130425c48cb720021130fdc7580d85610a4
Henrique Cruz <eu@henriquecruz.com.br>
2020-12-09 12:35:59 -0300

    Modules and libs update

--- functions/script.php
+++ functions/script.php
@@ -17,24 +17,13 @@ function gulp_wp_theme_scripts() {
 	/**
 	 * Enqueue common scripts.
 	 */
-	/*wp_register_script( $theme_handle_prefix . '-scripts', get_template_directory_uri() . '/assets/js/' . $theme_handle_prefix . '.min.js', array( 'jquery' ), '1.0.0', true );
-	wp_enqueue_script( $theme_handle_prefix . '-scripts' );
-
-	wp_register_script( 'infinite-scroll', get_template_directory_uri() . '/assets/js/libs/infinite-scroll.min.js', array('jquery'), '3.0.6', true );
-	wp_register_script( 'fancybox', get_template_directory_uri() . '/assets/js/libs/jquery.fancybox.min.js', array('jquery'), '3.5.7', true );
-	
-	
-	wp_enqueue_script( 'infinite-scroll' );
-	wp_enqueue_script( 'fancybox' );*/
-
-
 	wp_register_script( $theme_handle_prefix . '-scripts', get_template_directory_uri() . '/assets/js/theme.min.js', array( 'jquery' ), '1.0.0', true );
 	wp_enqueue_script( $theme_handle_prefix . '-scripts' );
 
-	wp_register_script( 'slick', get_template_directory_uri() . '/assets/js/libs/slick.min.js', array('jquery'), '3.0.6', true );
+	wp_register_script( 'tiny-slider', get_template_directory_uri() . '/assets/js/libs/tiny-slider.min.js', array('jquery'), '2.9.3', true );
 	wp_register_script( 'fancybox', get_template_directory_uri() . '/assets/js/libs/jquery.fancybox.min.js', array('jquery'), '3.0.6', true );
 	wp_register_script( 'infinite-scroll', get_template_directory_uri() . '/assets/js/libs/infinite-scroll.pkgd.min.js', array('jquery'), '3.0.6', true );
-	wp_enqueue_script( 'slick' );
+	wp_enqueue_script( 'tiny-slider' );
 	wp_enqueue_script( 'fancybox' );
 
 	wp_localize_script( $theme_handle_prefix . '-scripts', 'global_vars',
--- functions/style.php
+++ functions/style.php
@@ -15,34 +15,11 @@ function gulp_wp_theme_styles() {
 	global $theme_handle_prefix;
 
 	wp_register_style( $theme_handle_prefix . '-styles', get_template_directory_uri() . '/assets/css/style.min.css', array(), '1.0.0', 'all' );
-	wp_register_style( 'about-styles', get_template_directory_uri() . '/assets/css/layout/pages/about.min.css', array(), '1.0.0', 'all' );
-	wp_register_style( 'dealers-styles', get_template_directory_uri() . '/assets/css/layout/pages/dealers.min.css', array(), '1.0.0', 'all' );
-	wp_register_style( 'contact-styles', get_template_directory_uri() . '/assets/css/layout/pages/contact.min.css', array(), '1.0.0', 'all' );
-	wp_register_style( 'archive-styles', get_template_directory_uri() . '/assets/css/layout/pages/archive.min.css', array(), '1.0.0', 'all' );
-	wp_register_style( 'single-post-styles', get_template_directory_uri() . '/assets/css/layout/pages/single.min.css', array(), '1.0.0', 'all' );
-	wp_register_style( 'slick', get_template_directory_uri() . '/assets/css/libs/slick.css', array(), '3.5.7', 'all' );
-	wp_register_style( 'slick-theme', get_template_directory_uri() . '/assets/css/libs/slick-theme.css', array(), '3.5.7', 'all' );
+	wp_register_style( 'tiny-slider', get_template_directory_uri() . '/assets/css/libs/tiny-slider.min.css', array(), '2.3.4', 'all' );
 	wp_register_style( 'fancybox', get_template_directory_uri() . '/assets/css/libs/jquery.fancybox.min.css', array(), '3.5.7', 'all' );
 	
 	wp_enqueue_style( $theme_handle_prefix . '-styles' );
-	wp_enqueue_style( 'slick' );
-	wp_enqueue_style( 'slick-theme' );
 	wp_enqueue_style( 'fancybox' );
-
-	if(is_page('quem-somos'))
-		wp_enqueue_style( 'about-styles' );
-
-	if(is_page('dealers'))
-		wp_enqueue_style( 'dealers-styles' );
-
-	if(is_page('fale-conosco'))
-		wp_enqueue_style( 'contact-styles' );
-
-	if(is_archive())
-		wp_enqueue_style( 'archive-styles' );
-
-	if(is_singular(array('post', 'eventos')))
-		wp_enqueue_style( 'single-post-styles' );
 
 }
 add_action( 'wp_enqueue_scripts', 'gulp_wp_theme_styles' );
